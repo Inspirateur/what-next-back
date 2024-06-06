12d12fab6b1f988bdc517efde9cc4995cfd121f6
Téo ORTHLIEB <teo.orthlieb@kaizen-solutions.net>
2024-06-06 11:49:11 +0200

    fixed cors issues

---
+++ src/cors.rs
@@ -0,0 +1,22 @@
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _req: &'r Request<'_>, res: &mut Response<'r>) {
+        res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        res.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS",));
+        res.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        res.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+mod cors;
 use std::error::Error;
-use rocket::{get, http::{Method, Status}, post, routes, serde::json::Json};
-use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use cors::CORS;
+use rocket::{get, http::Status, options, post, routes, serde::json::Json};
 use rusqlite::Connection;
 use strum::IntoEnumIterator;
 use api_models::*;
 use jwt_auth::*;
 use what_next_back::*;
+
+// CORS boilerplate that really should be handled by Rocket but is unfortunately not.
+#[options("/signup")]
+fn _sign_up() {}
 
 #[post("/signup", format = "application/json", data = "<req>")]
 fn sign_up(req: Json<CredentialRequest>) -> Result<String, Status> {
@@ -16,6 +21,9 @@ fn sign_up(req: Json<CredentialRequest>) -> Result<String, Status> {
     })?;
     Ok(create_jwt(user_id).map_err(|_| Status::InternalServerError)?)
 }
+
+#[options("/login")]
+fn _login() {}
 
 #[post("/login", format = "application/json", data = "<req>")]
 fn login(req: Json<CredentialRequest>) -> Result<String, Status> {
@@ -28,6 +36,9 @@ fn login(req: Json<CredentialRequest>) -> Result<String, Status> {
     Ok(create_jwt(user_id).map_err(|_| Status::InternalServerError)?)
 }
 
+#[options("/change_password")]
+fn _change_pwd() {}
+
 #[post("/change_password", format = "application/json", data = "<req>")]
 fn change_pwd(req: Json<ChangePasswordRequest>) -> Result<String, Status> {
     let conn = establish_connection(DatabaseKind::PROD).map_err(|_| Status::InternalServerError)?;
@@ -38,13 +49,15 @@ fn change_pwd(req: Json<ChangePasswordRequest>) -> Result<String, Status> {
         })?;
     Ok(create_jwt(user_id).map_err(|_| Status::InternalServerError)?)
 }
+#[options("/media")]
+fn _media() {}
 
 #[get("/media")]
 fn media() -> Result<String, Status> {
     serde_json::to_string(&Medium::iter().collect::<Vec<_>>()).map_err(|_| Status::InternalServerError)
 }
 
-fn _reco(conn: &Connection, user_id: i32, medium: Medium) -> Result<String, Status> {
+fn reco_worker(conn: &Connection, user_id: i32, medium: Medium) -> Result<String, Status> {
     let Some(reco) = get_reco(&conn, user_id, medium).map_err(|_| Status::InternalServerError)? else {
         return Err(Status::NotFound);
     };
@@ -56,11 +69,18 @@ fn _reco(conn: &Connection, user_id: i32, medium: Medium) -> Result<String, Stat
     Ok(serde_json::to_string(&oeuvre).map_err(|_| Status::InternalServerError)?)
 }
 
+
+#[options("/reco")]
+fn _reco() {}
+
 #[post("/reco", format = "application/json", data = "<medium>")]
 fn reco(jwt: JWT, medium: Json<Medium>) -> Result<String, Status> {
     let conn = establish_connection(DatabaseKind::PROD).map_err(|_| Status::InternalServerError)?;
-    _reco(&conn, jwt.claims.user_id, medium.0)
+    reco_worker(&conn, jwt.claims.user_id, medium.0)
 }
+
+#[options("/rate_reco")]
+fn _rate_reco() {}
 
 #[post("/rate_reco", format = "application/json", data = "<req>")]
 fn rate_reco(jwt: JWT, req: Json<RateRequest>) -> Result<String, Status> {
@@ -68,8 +88,11 @@ fn rate_reco(jwt: JWT, req: Json<RateRequest>) -> Result<String, Status> {
     update_user_rating(&conn, jwt.claims.user_id, req.oeuvre_id, req.rating)
         .map_err(|_| Status::InternalServerError)?;
     let medium = get_oeuvre(conn, req.oeuvre_id).map_err(|_| Status::InternalServerError)?.medium;
-    _reco(&conn, jwt.claims.user_id, medium)
+    reco_worker(&conn, jwt.claims.user_id, medium)
 }
+
+#[options("/rate")]
+fn _rate() {}
 
 #[post("/rate", format = "application/json", data = "<req>")]
 fn rate(jwt: JWT, req: Json<RateRequest>) -> Result<(), Status> {
@@ -79,6 +102,9 @@ fn rate(jwt: JWT, req: Json<RateRequest>) -> Result<(), Status> {
     Ok(())
 }
 
+#[options("/unrate")]
+fn _unrate() {}
+
 #[post("/unrate", format = "application/json", data = "<oeuvre_id>")]
 fn unrate(jwt: JWT, oeuvre_id: Json<i32>) -> Result<(), Status> {
     let conn = establish_connection(DatabaseKind::PROD).map_err(|_| Status::InternalServerError)?;
@@ -87,6 +113,9 @@ fn unrate(jwt: JWT, oeuvre_id: Json<i32>) -> Result<(), Status> {
     Ok(())
 }
 
+#[options("/rated")]
+fn _rated() {}
+
 #[get("/rated")]
 fn rated(jwt: JWT) -> Result<String, Status> {
     // TODO: return all the oeuvre the user has rated, so they can see and possibily edit the ratings
@@ -94,6 +123,9 @@ fn rated(jwt: JWT) -> Result<String, Status> {
     let oeuvres = get_rated_oeuvres(&conn, jwt.claims.user_id).map_err(|_| Status::InternalServerError)?;
     serde_json::to_string(&oeuvres).map_err(|_| Status::InternalServerError)
 }
+
+#[options("/search/<_>/<_>")]
+fn _search() {}
 
 #[get("/search/<medium>/<query>")]
 fn search(jwt: JWT, medium: &str, query: &str) -> Result<String, Status> {
@@ -104,20 +136,12 @@ fn search(jwt: JWT, medium: &str, query: &str) -> Result<String, Status> {
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let allowed_origins = AllowedOrigins::all();
-
-    // You can also deserialize this
-    let cors = rocket_cors::CorsOptions {
-        allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::all(),
-        allow_credentials: true,
-        ..Default::default()
-    }.to_cors()?;
-
     let _ = rocket::build()
-        .mount("/", routes![sign_up, login, change_pwd, reco, rate, rate_reco, unrate, media, rated])
-        .attach(cors)
+        .attach(CORS)
+        .mount("/", routes![
+            sign_up, login, change_pwd, reco, rate, rate_reco, unrate, media, rated,
+            _sign_up, _login, _change_pwd, _reco, _rate, _rate_reco, _unrate, _media, _rated,
+        ])
         .launch()
         .await?;
 
